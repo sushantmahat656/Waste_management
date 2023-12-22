7248f260a1573ba4643d51bce95d79b6395d9c11
sushantmahat656 <sushantmahat656@gmail.com>
2023-12-22 15:00:45 +0545

    Login commit 2

--- waste_reduction_buddy/urls.py
+++ waste_reduction_buddy/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.home, name='home'),
     #path('login/',views.login_user,name='login'),
-    #path('logout/',views.logout_user,name='logout'),  
+    path('logout/',views.logout_user,name='logout_user'),  
 ]
--- waste_reduction_buddy/views.py
+++ waste_reduction_buddy/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render,redirect 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
 def home(request):
-    return render(request,'home.html',{})
+    #to check logging in
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticatiom
+        user =authenticate(request,username = username,password =password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You have been Logged in!!")
+            return redirect('home')
+        else:
+            messages.success(request,"Error logging in.Re-try again later...")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been Logged out ...")
+    return redirect('home')
+

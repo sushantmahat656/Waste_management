5f6fb9ba41814e743c401bb8c0a5b50c338d49f2
sushantmahat656 <sushantmahat656@gmail.com>
2023-12-26 16:03:15 +0545

    Registeration completed

--- waste_reduction_buddy/views.py
+++ waste_reduction_buddy/views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -33,14 +34,18 @@ def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            #Authentication
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user =authenticate(username = username, password= password)
-            login(request, user)
-            messages.success(request,"You have successfully Registered")
-            return redirect('login')
+            try:
+                form.save()
+                # Authentication
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password1']
+                user = authenticate(request, username=email, password=password)
+                login(request, user)
+                messages.success(request, "You have successfully registered.")
+                return redirect('login_user')
+            except IntegrityError:
+                messages.error(request, "This email address is already in use. Please use a different email.")
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})

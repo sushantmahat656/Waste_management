2824788eb56cf1b9cdd948a35d4a744b02a4c78c
sushantmahat656 <sushantmahat656@gmail.com>
2023-12-26 15:04:18 +0545

    Registeration

--- waste_reduction_buddy/views.py
+++ waste_reduction_buddy/views.py
@@ -1,30 +1,32 @@
-from django.shortcuts import render,redirect 
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages 
+from django.contrib import messages
 
 def home(request):
-    #to check logging in
-    if request.method =='POST':
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        #authenticatiom
-        user =authenticate(request,username = username,password =password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            messages.success(request,"You have been Logged in!!")
-            return redirect('home')
+            messages.success(request, "You have been Logged in!!")
+            email_domain = username.split('@')[-1]
+            return render(request, 'home.html', {'email_domain': email_domain})
+            #return redirect('home')
         else:
-            messages.success(request,"Error logging in.Re-try again later...")
+            messages.success(request, "Error logging in. Re-try again later...")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
-
-
-
+        return render(request, 'login.html', {})
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been Logged out ...")
     return redirect('home')
 
+def register_user(request):
+    return render(request, 'register.html', {})
